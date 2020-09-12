0543b2eebe1776f75f4094e02b2e419ebb1aa828
OscarNLA <a2019114151@estudiantes.upsa.edu.bo>
2020-09-12 11:51:11 -0400

    productos

---
+++ ProductoEnum/ProductoEnum/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProductoEnum.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<ProductoEnum.Models.Product> Products { get; set; }
+    }
+}
---
+++ ProductoEnum/ProductoEnum/Models/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace ProductoEnum.Models
+{
+    public enum CategoryType
+    { 
+    
+      Electronicos=10,
+      Alimenticios=20,
+      Moda=30,
+      Belleza=40,
+      Automotriz=50,
+      Libros=60,
+      caballos=70,
+    }
+
+    public class Product
+    {
+        [Key]
+       public int ProductId { get; set; }
+        [Display(Name = "nombre del producto")]
+        [Required(ErrorMessage ="Debe ingresar el nombre del producto")]
+        public string Name { get; set; }
+        [Display(Name = "Cateegoria del producto")]
+        [Required(ErrorMessage = "Debe ingresar la categoríá del producto")]
+        public CategoryType Category { get; set; }
+    }
+}
